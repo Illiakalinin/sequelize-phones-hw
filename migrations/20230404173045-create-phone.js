3bbcf866e78aa98b5e632a270d3ca2ea262ac711
illiakalinin <illinoiz4305@gmail.com>
2023-04-06 22:24:57 +0300

    Add migration

---
+++ migrations/20230404173045-create-phone.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Phones', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      brand: {
+        type: Sequelize.STRING,
+      },
+      model: {
+        type: Sequelize.STRING,
+      },
+      manufacturedYear: {
+        type: Sequelize.DATEONLY,
+      },
+      ram: {
+        type: Sequelize.INTEGER,
+      },
+      cpu: {
+        type: Sequelize.STRING,
+      },
+      displaySize: {
+        type: Sequelize.FLOAT,
+      },
+      nfc: {
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Phones');
+  },
+};
